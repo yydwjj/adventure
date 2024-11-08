da7b5d5bc5524c3050f21593d69021f104c1fec0
围巾 <2827348531@qq.com>
2024-11-08 15:28:40 +0800

    增加 返回结果类 用于controller层的使用

---
+++ src/main/java/com/yydwjj/adventure/result/ResultCodeEnum.java
@@ -0,0 +1,24 @@
+package com.yydwjj.adventure.result;
+
+import lombok.Getter;
+
+/**
+ * 统一返回结果状态信息类
+ *
+ */
+@Getter
+public enum ResultCodeEnum {
+
+    SUCCESS(200,"success"),
+    USERNAME_ERROR(501,"usernameError"),
+    PASSWORD_ERROR(503,"passwordError"),
+    NOTLOGIN(504,"notLogin"),
+    USERNAME_USED(505,"userNameUsed");
+
+    private Integer code;
+    private String message;
+    private ResultCodeEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
