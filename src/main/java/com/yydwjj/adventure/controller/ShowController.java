db9582832a313a2d347fe6ccea5ac0285eb06c04
围巾 <2827348531@qq.com>
2024-11-12 09:25:32 +0800

    实现 页面展示类

---
+++ src/main/java/com/yydwjj/adventure/controller/ShowController.java
@@ -0,0 +1,29 @@
+package com.yydwjj.adventure.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller()
+@RequestMapping("show")
+public class ShowController {
+    /**
+     * 重定向到首页
+     * @return
+     */
+    @RequestMapping(value = "index",method = RequestMethod.GET)
+    public String test() {
+//        System.out.println("aaa");
+        return "redirect:/indexNew.html";
+    }
+
+    /**
+     * 重定向到登录页
+     * @return
+     */
+    @RequestMapping(value = "login",method = RequestMethod.GET)
+    public String login() {
+//        System.out.println("aaa");
+        return "redirect:/login.html";
+    }
+}
