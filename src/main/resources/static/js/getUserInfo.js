d85620071691b13081381fde5e17415361228bc0
围巾 <2827348531@qq.com>
2024-11-12 16:23:15 +0800

    修改 将方法封装进getUserInfo()中，如果成功返回data数据

--- src/main/resources/static/js/getUserInfo.js
+++ src/main/resources/static/js/getUserInfo.js
@@ -1,24 +1,26 @@
 function getToken() {
     return localStorage.getItem('authToken');
 }
-
 // 获取用户信息
-const jwtToken = getToken();
+function getUserInfo() {
+    const jwtToken = getToken();
 
-fetch('/user/getUserInfo', {
-    method: 'GET',
-    headers: {
-        // 'Authorization': `Bearer ${jwtToken}`,
-        'Content-Type': 'application/json',
-        'token':`${jwtToken}`
-    }
-})
-    .then(response => response.json())
-    .then(data => {
-        // document.getElementById('response').innerText = JSON.stringify(data, null, 2);
-        console.log(data);
+    fetch('/user/getUserInfo', {
+        method: 'GET',
+        headers: {
+            // 'Authorization': `Bearer ${jwtToken}`,
+            'Content-Type': 'application/json',
+            'token':`${jwtToken}`
+        }
     })
-    .catch(error => {
-        console.error('Error:', error);
-        document.getElementById('response').innerText = 'Error: ' + error.message;
-    });
+        .then(response => response.json())
+        .then(data => {
+            // document.getElementById('response').innerText = JSON.stringify(data, null, 2);
+           return data;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            return null;
+        });
+}
+
