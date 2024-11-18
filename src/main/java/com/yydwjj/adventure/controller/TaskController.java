98d8c85577147cc14ee9559ba41864fd68c98be9
围巾 <2827348531@qq.com>
2024-11-18 10:38:31 +0800

    添加 task的添加、多个查询方法

--- src/main/java/com/yydwjj/adventure/controller/TaskController.java
+++ src/main/java/com/yydwjj/adventure/controller/TaskController.java
@@ -1,15 +1,17 @@
 package com.yydwjj.adventure.controller;
 
+import com.yydwjj.adventure.entity.Task;
 import com.yydwjj.adventure.entity.TaskCategorie;
 import com.yydwjj.adventure.entity.TaskLevel;
 import com.yydwjj.adventure.mapper.TaskCategoryMapper;
 import com.yydwjj.adventure.mapper.TaskLevelMapper;
 import com.yydwjj.adventure.result.Result;
+import com.yydwjj.adventure.service.TaskService;
+import com.yydwjj.adventure.utils.JwtHelper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 /**
@@ -24,6 +26,14 @@ public class TaskController {
     TaskLevelMapper taskLevelMapper;
     @Autowired
     TaskCategoryMapper taskCategoryMapper;
+    @Autowired
+    TaskService taskService;
+    @Autowired
+    JwtHelper jwtHelper;
+
+
+//    @Autowired
+//    TaskServer taskServer;
 
 
     @RequestMapping(value = "levels",method = RequestMethod.GET)
@@ -37,6 +47,49 @@ public class TaskController {
         List<TaskCategorie> category = taskCategoryMapper.getAll();
         return Result.ok(category);
     }
+
+    /**
+     * 发帖的方法
+     * @param task
+     * @param token
+     * @return
+     */
+    @RequestMapping(value = "post",method = RequestMethod.POST)
+    public Result postTask(@RequestBody Task task, @RequestHeader String token) {
+        Long userId = jwtHelper.getUserId(token);
+//        获取了用户id
+        task.setPublisherId(userId);
+        task.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+        task.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
+        return taskService.postTask(task);
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public Result getTask(@PathVariable int id, @RequestHeader String token) {
+        Long userId = jwtHelper.getUserId(token);
+        return taskService.getTask(id);
+    }
+
+    @RequestMapping(value = "all",method = RequestMethod.GET)
+    public Result getAllTasks(){
+        return taskService.getAllTasks();
+    }
+
+    @RequestMapping(value = "c/{categoryId}",method = RequestMethod.GET)
+    public Result getTasksByCategory(@RequestParam int categoryId){
+        return taskService.findTasksByCategoryId(categoryId);
+    }
+
+    @RequestMapping(value = "l/{levelId}",method = RequestMethod.GET)
+    public Result getTasksByLevel(@RequestParam int levelId){
+        return taskService.findTasksByLevelId(levelId);
+    }
+
+    @RequestMapping(value = "search/{keyword}",method = RequestMethod.GET)
+    public Result getTasksByKeyword(@PathVariable String keyword){
+        return taskService.searchTasks(keyword);
+    }
+
 
 
 }
