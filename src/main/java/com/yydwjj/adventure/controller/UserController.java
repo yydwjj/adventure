de3013a379f65e26f62c7fe4a483c1b9b03833b2
userName <userEmail>
2024-11-19 08:43:52 +0800

    修改了各种js

--- src/main/java/com/yydwjj/adventure/controller/UserController.java
+++ src/main/java/com/yydwjj/adventure/controller/UserController.java
@@ -1,17 +1,50 @@
 package com.yydwjj.adventure.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.yydwjj.adventure.entity.User;
+import com.yydwjj.adventure.result.Result;
+import com.yydwjj.adventure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
 
-    @PostMapping("test")
-    public String test(@RequestParam("username") String username, @RequestParam("password") String password) {
-        return "success";
+    @Autowired
+    UserService userService;
+
+    /**
+     * 用户的登录接口，若登陆成功，在result中包含一个<token,JWTtoken>,需要在客户端保存，并在之后的操作中，在请求头(RequestHead)中携带，用来识别用户
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = "login",method = RequestMethod.POST)
+    @ResponseBody
+    public Result test(@RequestBody User user) {
+        Result result = userService.login(user);
+        return result;
+    }
+
+    /**
+     * 用户的注册接口，需要携带用户的用户名，电话，密码
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = "register",method = RequestMethod.POST)
+    @ResponseBody
+    public Result register(@RequestBody User user) {
+        Result result = userService.register(user);
+        return result;
+    }
+
+    /**
+     * 获取操作用户信息的接口，需要在RequestHeader中携带JWTtoken
+     * @param token
+     * @return
+     */
+    @RequestMapping(value = "getUserInfo" ,method = RequestMethod.GET)
+    public Result getUserInfo(@RequestHeader String token) {
+        Result result = userService.getUserInfo(token);
+        return result;
     }
 }
