8e4bc4ceff2dbf354cfd50c0b927e5786076c473
围巾 <2827348531@qq.com>
2024-11-21 19:42:32 +0800

    更新 使用lombok注解代替get，set方法

--- src/main/java/com/yydwjj/adventure/entity/User.java
+++ src/main/java/com/yydwjj/adventure/entity/User.java
@@ -1,6 +1,8 @@
 package com.yydwjj.adventure.entity;
 
 
+import lombok.Data;
+@Data
 public class User {
 
   private long userId;
@@ -16,100 +18,4 @@ public class User {
   private String userPwd;
 
 
-  public long getUserId() {
-    return userId;
-  }
-
-  public void setUserId(long userId) {
-    this.userId = userId;
-  }
-
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-
-  public String getPhoneNumber() {
-    return phoneNumber;
-  }
-
-  public void setPhoneNumber(String phoneNumber) {
-    this.phoneNumber = phoneNumber;
-  }
-
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-
-  public long getSchoolId() {
-    return schoolId;
-  }
-
-  public void setSchoolId(long schoolId) {
-    this.schoolId = schoolId;
-  }
-
-
-  public String getNotes() {
-    return notes;
-  }
-
-  public void setNotes(String notes) {
-    this.notes = notes;
-  }
-
-
-  public java.sql.Timestamp getCreatedAt() {
-    return createdAt;
-  }
-
-  public void setCreatedAt(java.sql.Timestamp createdAt) {
-    this.createdAt = createdAt;
-  }
-
-
-  public java.sql.Timestamp getUpdatedAt() {
-    return updatedAt;
-  }
-
-  public void setUpdatedAt(java.sql.Timestamp updatedAt) {
-    this.updatedAt = updatedAt;
-  }
-
-
-  public java.sql.Timestamp getDeletedAt() {
-    return deletedAt;
-  }
-
-  public void setDeletedAt(java.sql.Timestamp deletedAt) {
-    this.deletedAt = deletedAt;
-  }
-
-
-  public long getUserAvatarId() {
-    return userAvatarId;
-  }
-
-  public void setUserAvatarId(long userAvatarId) {
-    this.userAvatarId = userAvatarId;
-  }
-
-  public String getUserPwd() {
-    return userPwd;
-  }
-
-  public void setUserPwd(String userPwd) {
-    this.userPwd = userPwd;
-  }
 }
