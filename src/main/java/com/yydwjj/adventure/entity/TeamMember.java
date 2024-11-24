aa17a0c706af44cab0d33635bbb3519adb02dd09
围巾 <2827348531@qq.com>
2024-11-24 16:22:20 +0800

    添加 TeamMember团队成员中的teamId

--- src/main/java/com/yydwjj/adventure/entity/TeamMember.java
+++ src/main/java/com/yydwjj/adventure/entity/TeamMember.java
@@ -1,57 +1,15 @@
 package com.yydwjj.adventure.entity;
 
+import lombok.Data;
 
+@Data
 public class TeamMember {
 
   private long teamMemberId;
   private long jobId;
   private long userId;
+  private long teamId;
   private long permissionLevel;
   private long isInTeam;
-
-
-  public long getTeamMemberId() {
-    return teamMemberId;
-  }
-
-  public void setTeamMemberId(long teamMemberId) {
-    this.teamMemberId = teamMemberId;
-  }
-
-
-  public long getJobId() {
-    return jobId;
-  }
-
-  public void setJobId(long jobId) {
-    this.jobId = jobId;
-  }
-
-
-  public long getUserId() {
-    return userId;
-  }
-
-  public void setUserId(long userId) {
-    this.userId = userId;
-  }
-
-
-  public long getPermissionLevel() {
-    return permissionLevel;
-  }
-
-  public void setPermissionLevel(long permissionLevel) {
-    this.permissionLevel = permissionLevel;
-  }
-
-
-  public long getIsInTeam() {
-    return isInTeam;
-  }
-
-  public void setIsInTeam(long isInTeam) {
-    this.isInTeam = isInTeam;
-  }
 
 }
